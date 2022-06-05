0d815e62890f12c835bd8be26d5bd2f8cc502690
sadimohammad65@gmail.com <sadimohammad65@gmail.com>
2022-06-05 17:04:29 +0600

    Home API

---
+++ Home_Rent/Buseness Logic Layer/services/OwnerService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Buseness_Entity_Layer;
+using Data_Entity_Layer;
+using Data_Entity_Layer.AccessFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buseness_Logic_Layer.services
+{
+    public class OwnerService
+    {
+        public static List<OwnerModel>GetAll()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Owner, OwnerModel>());
+            var mapper = new Mapper(config);
+            var data = mapper.Map<List<OwnerModel>>(DataAccessFactory.OwnerDataAccess().Get());
+            return data;
+        }
+
+        public static OwnerModel GetId(int id)
+        {
+            var config=new MapperConfiguration(cfg => cfg.CreateMap<Owner, OwnerModel>());          
+            var mapper=new Mapper(config);
+            var data=mapper.Map<OwnerModel>(DataAccessFactory.OwnerDataAccess().Get(id));
+            return data;
+
+        }
+
+        public static bool Edit(OwnerModel e)
+        {
+             
+
+            var data = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<OwnerModel, Owner>())).Map<Owner>(e);
+
+            try
+            {
+                 DataAccessFactory.OwnerDataAccess().Edit(data);
+                return true;    
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteOwner(int id)
+        {
+            try
+            {
+                DataAccessFactory.OwnerDataAccess().Delete(id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool AddOwner(OwnerModel e)
+        {
+            var data=new Mapper(new MapperConfiguration(cfg=>cfg.CreateMap<OwnerModel,Owner>())).Map<Owner>(e);
+
+            try
+            {
+                DataAccessFactory.OwnerDataAccess().Add(data);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Home_Rent/Home List Rent/Controllers/HouseOwnerController.cs
@@ -0,0 +1,42 @@
+﻿using Buseness_Entity_Layer;
+using Buseness_Logic_Layer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Home_List_Rent.Controllers
+{
+    public class HouseOwnerController : ApiController
+    {
+        [Route("api/Owner/list")]
+        [HttpGet]
+        public List<OwnerModel>Get()
+        {
+            return OwnerService.GetAll();
+        }
+    
+        [Route("api/Owner/Edit/{id}")]
+        [HttpPost]
+        public void EditOwner(OwnerModel n)
+        {
+            OwnerService.Edit(n);
+        }
+        [Route("api/Owner/Delete/{id}")]
+        [HttpGet]
+        public HttpResponseMessage DeleteOwner(int id)
+        {
+            bool sucess = OwnerService.DeleteOwner(id);
+            return Request.CreateResponse(sucess ? HttpStatusCode.OK : HttpStatusCode.BadRequest);
+        }
+        [Route("api/Investor/CreateInvestor")]
+        [HttpPost]
+        public void AddOwner(OwnerModel n)
+        {
+            OwnerService.AddOwner(n);
+        }
+
+    }
+}
